be1918b2f91798116655737b246aff952c20f8d9
Heli Sivunen <heli@mooncat.tv>
2020-09-28 18:46:21 +0100

    Getting there with links - currently hover working weirdly and second page is a skeleton.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,41 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
+import ProjectThree from "../components/ProjectThree"
 import "./styling.scss"
+import { Column, Row } from 'simple-flexbox';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h2>Projects</h2>
+class IndexPage extends Component{
+  render(){
+    return(
+      <Layout>
+  <SEO title="Home" />
+  <h2>Projects</h2>
 
-    <Link to="/page-2/">Team 6 Game</Link> <br />
-    <Link to="/using-typescript/">Headline Search</Link>
-  </Layout>
+  <Link to="/page-2/">Team 6 Game</Link> <br />
+  <Link to="/using-typescript/">Headline Search</Link>
 
-)
+      <Column>
+            <div class="container">
+
+                        <section class="cards">
+
+                            <article class="card">
+                            <div class="text">
+                              <p><ProjectThree /></p>
+                              </div>
+                            </article>
+
+                        </section>
+            </div>
+            </Column>
+</Layout>
+    )
+  }
+}
+
 
 export default IndexPage
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,42 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
-
-export default SecondPage
+// import React, { Component } from "react"
+// import { Link } from "gatsby"
+//
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
+//
+// class ProjectThreeDetails extends Component {
+//   constructor(props) {
+//     super(props)
+//       this.state = { starredUserData: [] }
+//     };
+//
+//   componentDidMount() {
+//     fetch("https://api.github.com/users/PacificRebel/starred")
+//     .then(response => response.json())
+//     .then(data => {
+//       this.setState({starredUserData: data})
+//     })
+//   }
+//
+//   render() {
+//     const data = this.state.starredUserData
+//     console.log(data)
+//     let name = ''
+//     let description = ''
+//     let homepage = ''
+//     if (data.length > 0) {
+//       name = data[1].name
+//       description = data[1].description
+//     }
+//
+//     return (
+//       <div className="projectthreedetails">
+//       <span
+//       {name}{description}{homepage}>Hello!
+//       </span>
+//       </div>
+//      )
+//    }
+//  }
+//
+//  export default ProjectThreeDetails

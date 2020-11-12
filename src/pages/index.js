781451db71ba2a4f519e2d27bd073ef0b7a4ecbd
Heli Sivunen <heli@mooncat.tv>
2020-11-12 15:22:03 +0000

    Remove unnecessary imports from index js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,4 @@
 import React, { Component } from "react"
-// import { Link } from "gatsby"
-
-// import Layout from "../components/layout"
-// import Image from "../components/image"
-// import SEO from "../components/seo"
 
 import ProjectOne from "../components/ProjectOne"
 import ProjectTwo from "../components/ProjectTwo"

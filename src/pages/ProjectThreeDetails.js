0ec4dd3f431613e9c6358f8e4a39aceb0abdb7ac
Heli Sivunen <heli@mooncat.tv>
2021-01-12 14:14:25 +0000

    Cleanup ProjectTreeDetails

--- src/pages/ProjectThreeDetails.js
+++ src/pages/ProjectThreeDetails.js
@@ -29,7 +29,8 @@ class ProjectThreeDetails extends Component {
     return (
       <div className="projectdetails">
       <span>
-      {name}{description}
+      {name}<p>{`
+        `}</p>{description}
       </span>
       <a href={homepage}>{homepage}</a>
       </div>
